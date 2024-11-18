f96ad196a649c0e9348c9ca8e21877d82242b823
Hans Jonathan Llanos Chavez <hllanosc@emeal.nttdata.com>
2024-11-18 08:21:18 -0500

    add config server

--- ms-config-server/src/main/java/com/configserver/ms_config_server/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/configserver/ms_config_server/MsConfigServerApplication.java
@@ -2,9 +2,10 @@ package com.configserver.ms_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnabledConfigServer
+@EnableConfigServer
 public class MsConfigServerApplication {
 
 	public static void main(String[] args) {
